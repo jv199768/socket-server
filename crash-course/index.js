2f087ffb06249abb1ce32cdd083e5fd81764640c
ShatilKhan <shatilshahriar009@gmail.com>
2024-12-27 07:40:49 +0600

    Websocket sever sends random number every 5 seconds
    Signed-off-by: ShatilKhan <shatilshahriar009@gmail.com>

--- crash-course/index.js
+++ crash-course/index.js
@@ -1,14 +1,20 @@
 const http = require("http");
-const WebSokcetServer = require("websocket").server
+const WebSocketServer = require("websocket").server
 let connection = null; //global var for connection, that will be changes later
 
 const httpserver = http.createServer((req, res) => {
     console.log("we have received a request");//first breakpoint, not really necessary just to check connection
 })
 
-const websocket = new WebSokcetServer({
+const websocket = new WebSocketServer({
     "httpServer": httpserver //Pass in the http server that we made,
 })
+
+
+httpserver.listen(8080, () =>
+    console.log("server listening on port 8080") //server starts listening for client connection
+);
+
 
 // On-Request Event where raw http server requests the websocket server connection
 // http server sends a UPGRADE 1.1 header that requests the websocket server to switch protocol to http 1.1 for TCP connection
@@ -22,10 +28,24 @@ websocket.on("request", request => {
     connection.on("open", () => console.log("OPEN"))
     connection.on("close", () => console.log("CLOSE"))
     connection.on("message", message => { // Fucntion to do something if server receives a message
+        //connection.send("message") from the debug console will send a message to the browser client
+        //on the browser console we can send a reply via : ws.send("Hello! I'm client")
+        //utf8 is the data type, after sending reply from client we can check the data using this breakpoint
         console.log(`Received message ${message.utf8Data}`)
     })
+    // to view message from the server using the browser client we can use this inside the browser console
+    //ws.onmessage = message => console.log(`We received server message: ${message.data}`)
+    sendevery5seconds(); //calling the function to send msg from server to client  every 5 sec
 })
 
-httpserver.listen(8080, () =>
-    console.log("server listening on port 8080")
-);
+
+
+// A new function that sends a message to the client every 5 seconds
+// This is specially useful for stuff like video game streaming
+function sendevery5seconds() {
+    if (connection) {
+        connection.send(`Message ${Math.random()}`);
+        setTimeout(sendevery5seconds, 5000);
+    }
+
+}
